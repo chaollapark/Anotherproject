64c232237dff8ed612c396ed30f349d7c1af3ac6
mouise1111 <mouise1111@gmail.com>
2024-11-13 00:11:31 +0100

    Fixed linting error

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -3,8 +3,10 @@ import { fetchEvents } from '@/models/Event';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const count = parseInt(searchParams.get('count') || '0');
+
+  //const count = parseInt(searchParams.get('count') || '0');
   
-  const events = await fetchEvents(10, count);
+  //const events = await fetchEvents(10, count);
+  const events = await fetchEvents(10);
   return NextResponse.json(events);
 }
