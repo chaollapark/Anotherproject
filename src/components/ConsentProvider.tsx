5073df69d391e79628169152c024a4441329fe87
Chaolla Park <chaollapark@gmail.com>
2025-03-21 17:13:47 +0100

    activation consent mode

--- src/components/ConsentProvider.tsx
+++ src/components/ConsentProvider.tsx
@@ -3,11 +3,19 @@
 import { useEffect } from 'react';
 import klaroConfig from '@/klaro-config';
 
+declare global {
+  interface Window {
+    klaroConfig: any;
+  }
+}
+
 export default function ConsentProvider() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
+      window.klaroConfig = klaroConfig;
       import('klaro').then((klaro: any) => {
         klaro.setup(klaroConfig);
+        klaro.show();
       });
     }
   }, []);
