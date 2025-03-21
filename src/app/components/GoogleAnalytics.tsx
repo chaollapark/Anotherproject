16ee55d8c0eb934b20045ce8f18f1b7be1aeb0c0
Chaolla Park <chaollapark@gmail.com>
2025-03-21 18:58:16 +0100

    google compliance

--- src/app/components/GoogleAnalytics.tsx
+++ src/app/components/GoogleAnalytics.tsx
@@ -5,17 +5,28 @@ import Script from 'next/script';
 export default function GoogleAnalytics() {
   return (
     <>
+      {/* Load GA script early */}
       <Script
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-G616KE1CWQ"
       />
+      {/* Setup Consent Mode and default consent */}
       <Script
         id="google-analytics"
-        strategy="afterInteractive"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
+            
+            // Set default consent to denied until user accepts via Klaro
+            gtag('consent', 'default', {
+              ad_storage: 'denied',
+              analytics_storage: 'denied',
+              ad_user_data: 'denied',
+              ad_personalization: 'denied'
+            });
+
             gtag('js', new Date());
             gtag('config', 'G-G616KE1CWQ');
           `,
@@ -23,4 +34,4 @@ export default function GoogleAnalytics() {
       />
     </>
   );
-} 
+}
