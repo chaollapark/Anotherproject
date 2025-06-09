cd5ce736b152b37bf75be25a5ceebf0710eb2c9b
Chaolla Park <chaollapark@gmail.com>
2025-06-09 19:22:00 +0200

    new post a job button

---
+++ src/app/components/FloatingPostJobButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { FaPlus } from 'react-icons/fa'; // Using react-icons for a plus icon
+
+export default function FloatingPostJobButton() {
+  const pathname = usePathname();
+
+  if (pathname === '/new-listing/form') {
+    return null; // Don't render the button on the form page
+  }
+
+  return (
+    <div className="hidden md:block fixed top-1/2 right-8 -translate-y-1/2 z-50">
+      <Link
+        href="/new-listing/form"
+        className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-3 px-6 rounded-lg shadow-lg transition-all duration-300 ease-in-out flex flex-col items-center text-center transform hover:scale-105"
+      >
+        <div className="flex items-center">
+          <FaPlus className="mr-2 h-5 w-5" />
+          <span>Post a Job</span>
+        </div>
+        <span className="text-xs mt-1 font-normal">With EUjobs it&apos;s only 50 euros</span>
+      </Link>
+    </div>
+  );
+}
